1b03601056cb3d3995028f683930cedae6eba47c
Prabeesh Prabhakar <pprabeesh@gmail.com>
2020-02-01 13:24:17 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Beer Mover
+
+This is a vanila single page application for monitoring a truckload of beer containers
---
+++ client/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>hdsfsdfsdklfsdlfsdlkfnsdlfnsldnflsdn</div>;
+};
+
+export default App;
